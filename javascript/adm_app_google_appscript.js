47fad91fb2f5f69b29144eb7bab4d4c2c2e966a5
FREDRICK OMONDI <66562017+afune19@users.noreply.github.com>
2024-11-24 21:47:57 +0300

    Add files via upload

---
+++ javascript/adm_app_google_appscript.js
@@ -0,0 +1,11 @@
+const scriptURL ='https://script.google.com/macros/s/AKfycbyX1t62EvDFRNBaEoQrNqbo75tMjSuF8MRsPBB80imZ7ek-_JpT71P4b9LnM3GnH70/exec'
+
+const form = document.forms['admission-form']
+
+form.addEventListener('submit', e => {
+e.preventDefault()
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+.then(response => alert("Thank you! your form is submitted successfully."))
+.then(() => { window.location.reload(); })
+.catch(error => console.error('Error!', error.message))
+})
---
+++ javascript/contact_popup.js
@@ -0,0 +1,9 @@
+// Function to open the popup
+function openPopup() {
+    document.getElementById("contact-popup").style.display = "block";
+}
+
+// Function to close the popup
+function closePopup() {
+    document.getElementById("contact-popup").style.display = "none";
+}
